ffb38b640b32c38c9fc21e5b5f34d8fafabbf08b
Soniya Nishad <soniyanishad10@gmail.com>
2025-02-08 16:58:10 +0530

    Add files via upload

---
+++ ass1.js
@@ -0,0 +1,83 @@
+//Q1.
+
+let a=20;
+let b=76;
+
+console.log("Addition of A(20)+B(76) is:", a+b);
+
+//Q2.
+
+let a2=5;
+let b2=3;
+
+if(a2<10<10 && a2>b2)
+{console.log(true);
+}
+else{
+    console.log(false);
+    
+}
+
+
+//Q3.
+
+let x=10;
+let y=69;
+
+if(x%10===0 || y%10===0 ){
+    console.log(true);  
+}
+else{
+    console.log(false);  
+}
+
+
+
+//Q4.
+
+let n=8756;
+
+let res=Math.floor(n/1000);
+
+console.log("First Digit Of Number N=8756 is",res);
+
+//Q5.
+
+let num=3672;
+
+let res1=Math.floor(num%10);
+
+console.log("Last Digit of Number N=3672 is",res1);
+
+//Q6.
+
+let p=78;
+let q=8;
+
+let res2=p%q;
+console.log("Remainder of P=78 and Q=8 are", res2);
+
+//Q7.
+
+let c=9;
+let d=67;
+
+let res3=c*d;
+console.log("Multiplication of c=9 and d=67 is",res3);
+
+//Q8.
+
+let a1=78;
+let b1=89;
+let c1=95;
+
+let sum=a1+b1+c1;
+console.log("Shyam's total marks of 3 subjects a1=78,b1=89,c1=95 is:",sum);
+
+let avg=(a1+b1+c1)/3;
+console.log("Shyam's Average marks of 3 subjects a1=78,b1=89,c1=95 is:",avg);
+
+
+
+
+
